18b3880de95138ca3aef9183a40888069fa854df
Markus Hedvall <mackanhedvall@gmail.com>
2019-12-02 20:52:01 +0100

    Fix windows support

--- conanfile.py
+++ conanfile.py
@@ -35,6 +35,7 @@ class VioletConan(ConanFile):
 
     def package(self):
         self.copy("*.h", dst="include", src="src", excludes="*/pch.h")
+        self.copy("*violet.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
